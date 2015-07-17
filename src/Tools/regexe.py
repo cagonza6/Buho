fd562e2e4911743220399827d357f48c3c153ab8
Anubis <cagonza6@gmail.com>
2015-07-17 21:28:23 +0200

    Separado regex validar en tipos, asi se podrán agregar filtros de mejor forma
    Reconoce las ñ

--- src/Tools/regexe.py
+++ src/Tools/regexe.py
@@ -1,17 +1,54 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
 import re
 
+def validar_nombre(texto,patron="^([A-Z]{0,1}[a-zñáéíóú]+[\s]*)+$"):
+	patron = re.compile(patron)
+	texto  = texto.strip()
+	#texto=texto.title()
+	if texto == '':
+		print "\t X"
+		return False
+	if 'nombre' in texto.lower():
+		print "\t X"
+		return False
+	if 'apellido' in texto.lower():
+		print "\t X"
+		return False
+
+	if patron.match(texto):
+		return texto
+	return False
+
+def validar_rut(texto,patron="^\d{6,}-[K|k|0-9]{1}$"):
+	patron = re.compile(patron)
+	texto  = texto.strip()
+
+	if patron.match(texto):
+		return True
+
+	return False
+
 def validar(tipo,valor):
+	tipo=tipo.strip()
 	valor=valor.strip()
 	if 'rut' in tipo:
 		patron="^\d{6,}-[K|k|0-9]{1}$"
+		return validar_rut(valor,patron)
 	elif 'name' in tipo.lower():
-		patron = "^([A-Z]{1}[a-zñáéíóú]+[\s]*)+$"
-	else:
-		return False
+		patron = "^([A-Z]{0,1}[a-zñáéíóú]+[\s]*)+$"
+		return validar_nombre(valor,patron)
 
-	if re.match(patron,valor):
-		return True
 	return False
+
+
+if __name__ == '__main__':
+
+	validar('name','Naño')
+	validar('name','Nano')
+	validar('name','Pancho')
+	validar('name','Juan Psr ico')
+	print validar('rut','111a1111-7')
+	print validar('rut','7-7')
+	print validar('rut','7999877-7')
+
