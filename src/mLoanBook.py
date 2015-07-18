95a6bf40e76fcf4b009a7e6b547dcbc2f2796a9f
Anubis <cagonza6@gmail.com>
2015-07-18 18:29:20 +0200

    Loan books toma los parametros como corresponde y guarda las variables

--- src/mLoanBook.py
+++ src/mLoanBook.py
@@ -18,14 +18,17 @@ class LoanBook(wx.Panel):
 		self.us_id = -1
 		fgs = wx.FlexGridSizer(2,2,7,15)
 		ste = wx.StaticText(self,label = "")
+
+		#Botones
 		btBk = wx.Button(self, label = "Buscar Libro")
-		self.tcBk = wx.TextCtrl(self)
 		btUs = wx.Button(self, label = "Buscar Usuario")
+
+		self.tcBk = wx.TextCtrl(self)
 		self.tcUs = wx.TextCtrl(self)
 		fgs.AddGrowableCol(1)
 		fgs.AddMany([(btBk, 0),(self.tcBk, 1, wx.EXPAND),(btUs, 0),(self.tcUs, 1, wx.EXPAND)])
 		#fgs.AddGrowableCol(1, 0)	#me asegura que crezcan como deben
-	
+
 		self.stDt = wx.StaticText(self, label = "Libro será prestado hasta el " + cfg.calc_return_date())
 		btPt = wx.Button(self, label = "Prestar")
 		
@@ -45,17 +48,17 @@ class LoanBook(wx.Panel):
 	def OnSelecUser(self, e):
 		mSearchWindows.SearchUser(self,self.users)
 
-	def RecieveIdn(self, idn, what):
+	def RecieveIdn(self, data, tipo):
 		#print idn, what
-		if what == 0:
-			self.bk_id = idn
-			self.tcBk.SetValue(cfg.bks[idn].GetTitle())
-			
-		if what == 1:
-			self.us_id = idn
-			self.tcUs.SetValue(cfg.uss[idn].GetName())
+		if tipo == 'book':
+			self.book = data
+			self.tcBk.SetValue(self.book['titulo'])
+		if tipo == 'user':
+			self.user = data
+			self.tcUs.SetValue(self.user['nombres']+" "+self.user['apellidos'])
 
 	def OnLoan(self, e):
+		print "Prestando"
 		if (self.bk_id != -1) and (self.us_id != -1):
 			bk = cfg.bks[self.bk_id]
 			us = cfg.uss[self.us_id]
@@ -73,16 +76,6 @@ class LoanBook(wx.Panel):
 	def Clean(self):
 		self.tcBk.SetValue("")
 		self.tcUs.SetValue("")
-
-#class Example(wx.Frame):
-	#def __init__(self,parent):
-		#super(Example, self).__init__(parent=parent, size=(700, 250)) 
-		#vbox = wx.BoxSizer(wx.VERTICAL)
-		#self.panel = NewBook(self)
-		#vbox.Add(self.panel,2,wx.EXPAND)
-		#self.SetSizer(vbox)
-		#self.Centre()
-		#self.Show()
 
 if __name__ == '__main__':
 	ex = wx.App()
