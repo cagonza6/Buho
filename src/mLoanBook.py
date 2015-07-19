1884c793040f41089a4f7f72ece1f6f6b63e7077
Maite <maite.barros@gmail.com>
2015-07-19 18:25:35 -0300

    Prestamos muestra datos de libro y usuario

--- src/mLoanBook.py
+++ src/mLoanBook.py
@@ -15,8 +15,6 @@ class LoanBook(wx.Panel):
 
 		wx.Panel.__init__(self, parent = parent, size = size)
 		vbox = wx.BoxSizer(wx.VERTICAL)
-		self.bk_id = -1
-		self.us_id = -1
 		fgs = wx.FlexGridSizer(2,2,7,15)
 		ste = wx.StaticText(self,label = "")
 
@@ -26,9 +24,54 @@ class LoanBook(wx.Panel):
 
 		self.tcBk = wx.TextCtrl(self)
 		self.tcUs = wx.TextCtrl(self)
+		fgs.AddMany([(btBk, 0), (self.tcBk, 1, wx.EXPAND),(btUs, 0), (self.tcUs, 1, wx.EXPAND)])
 		fgs.AddGrowableCol(1)
-		fgs.AddMany([(btBk, 0),(self.tcBk, 1, wx.EXPAND),(btUs, 0),(self.tcUs, 1, wx.EXPAND)])
-		#fgs.AddGrowableCol(1, 0)	#me asegura que crezcan como deben
+
+		#Paneles datos
+		bsDt = wx.BoxSizer(wx.HORIZONTAL)
+		
+		#Panel datos Usuario
+		self.pnlUs = wx.Panel(self, -1)
+		fgsUs   = wx.FlexGridSizer(3,2,7,15)
+		# Identificadores
+		stNm = wx.StaticText(self.pnlUs, label = "Nombre(s)")
+		stAp = wx.StaticText(self.pnlUs, label = "Apellido(s)")
+		stSt = wx.StaticText(self.pnlUs, label = "Estado")
+		# Campos
+		self.laNm = wx.StaticText(self.pnlUs)
+		self.laAp = wx.StaticText(self.pnlUs)
+		self.laSt = wx.StaticText(self.pnlUs)
+
+		fgsUs.AddMany([(stNm),(self.laNm, 0),
+		            (stAp),(self.laAp , 0),
+		            (stSt),(self.laSt , 0),
+		            ]
+		)
+		
+		self.pnlUs.SetSizer(fgsUs, 0)
+		self.pnlUs.Hide()
+		
+		#Datos Libro
+		self.pnlBk = wx.Panel(self, -1)
+		fgsBk = wx.FlexGridSizer(4,2,7,15)
+		# Identificadores
+		stIs  = wx.StaticText(self.pnlBk, label = "ISBN: ")
+		stTi  = wx.StaticText(self.pnlBk, label = "Título: ")
+		stAu  = wx.StaticText(self.pnlBk, label = "Autor: ")
+		stPt  = wx.StaticText(self.pnlBk, label = "Prestado: ")
+		# Campos
+		self.stIso = wx.StaticText(self.pnlBk, label =".")
+		self.stTio = wx.StaticText(self.pnlBk)
+		self.stAuo = wx.StaticText(self.pnlBk)
+		self.stPto = wx.StaticText(self.pnlBk)
+		fgsBk.AddMany([(stIs, 0),(self.stIso, 0),
+		             (stTi, 0),(self.stTio, 0),
+		             (stAu, 0),(self.stAuo, 0),
+		             (stPt, 0),(self.stPto, 0)])
+		self.pnlBk.SetSizer(fgsBk)
+		self.pnlBk.Hide()
+		
+		bsDt.AddMany([(self.pnlBk, 1), (wx.StaticLine(self, -1, style=wx.LI_VERTICAL),1,wx.ALIGN_CENTER_HORIZONTAL ),(self.pnlUs, 1)])
 
 		self.stDt = wx.StaticText(self, label = "Libro será prestado hasta el " + cfg.calc_return_date())
 		btPt = wx.Button(self, label = "Prestar")
@@ -38,6 +81,7 @@ class LoanBook(wx.Panel):
 		btPt.Bind(wx.EVT_BUTTON, self.OnLoan)
 			
 		vbox.Add(fgs, 0, wx.EXPAND)
+		vbox.Add(bsDt, 0, wx.EXPAND)
 		vbox.Add(self.stDt, 0)
 		vbox.Add(btPt, 0 , wx.EXPAND | wx.ALIGN_CENTER_VERTICAL)
 		self.SetSizer(vbox)
@@ -56,12 +100,35 @@ class LoanBook(wx.Panel):
 			self.book = self.validarLibro(data)
 			if self.book:
 				self.tcBk.SetValue(self.book['titulo'])
+				self.llenarDatosLibro()
 
 		elif tipo == 'user':
 			self.tcUs.SetValue('')
 			self.user = self.validarUser(data)
 			if self.user:
 				self.tcUs.SetValue(self.user['nombres']+" "+self.user['apellidos'])
+				self.llenarDatosUsuario()
+
+	def llenarDatosLibro(self):
+		
+		self.stIso.SetLabel(self.book['isbn'])
+		self.stTio.SetLabel(self.book['titulo'])
+		self.stAuo.SetLabel(self.book['autor'])
+		if self.book['estado']: self.stPto.SetLabel(label = "Si")
+		else: self.stPto.SetLabel("No")
+		self.pnlBk.Layout()
+		self.pnlBk.Show()
+		self.Layout()
+		
+	def	llenarDatosUsuario(self):
+		self.laNm.SetLabel(self.user['nombres'])
+		self.laAp.SetLabel(self.user['apellidos'])
+		if self.user['estado']: self.laSt.SetLabel("Activo")
+		else: self.laSt.SetLabel("Inactivo")
+		self.pnlUs.Layout()
+		self.pnlUs.Show()
+		self.Layout()
+
 
 	def validarUser(self, user):
 		if not ('estado' in user.keys()) or not user['estado']:
@@ -112,7 +179,24 @@ class LoanBook(wx.Panel):
 		self.tcBk.SetValue("")
 		self.tcUs.SetValue("")
 
+class DummyFrame(wx.Frame):
+	def __init__(self,parent):
+		super(DummyFrame, self).__init__(parent = parent, size=(700, 500)) 
+		vbox = wx.BoxSizer(wx.VERTICAL)
+		panel = LoanBook(self, self.GetSize())
+		vbox.Add(panel, 2, wx.EXPAND)
+		panel.Show()
+		self.SetSizer(vbox)
+		self.Centre()
+		self.Show()
+
+
 if __name__ == '__main__':
+	ddic1={'id_libro':1,'titulo':'Titulo animal','autor': 'Autor Auto 1','isbn':'123456789','comentarios': 'no hay 1','estado':0}
+	ddic2={'id_libro':2,'titulo':'Titulo mueble','autor': 'Autor Casa 2','isbn':'223456789','comentarios': 'no hay 2','estado':1}
+	ddic3={'id_libro':3,'titulo':'Titulo pelota','autor': 'Autor mono 2','isbn':'323456789','comentarios': 'no hay 3','estado':1}
+	books=[ddic1,ddic2,ddic3]
+
 	ex = wx.App()
-	LoanBook(None)
+	DummyFrame(None)
 	ex.MainLoop()    
